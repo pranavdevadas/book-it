613e5a94a53de7e91d242f1c56130fb12e78833b
pranavdevadas <pranavdevadas2@gmail.com>
2024-09-30 16:50:24 +0530

    updated vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {
-    port : 3000,
-    proxy : {
-      '/api' : {
-        target : 'http://localhost:5000',
-        changeOrigin : true
-      }
-    }
-  }
-})
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1600, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
+});
