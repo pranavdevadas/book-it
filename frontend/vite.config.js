ea9dbd7ea5b6b03d4af597f2513f85ad9694fedc
pranavdevadas <pranavdevadas2@gmail.com>
2024-10-02 15:45:25 +0530

    for hosting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:5000',
+    //     changeOrigin: true,
+    //   },
+    // },
   },
   build: {
     chunkSizeWarningLimit: 1600, 
