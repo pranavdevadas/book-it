a73c8114eddab7e0b1a6b56506bb93ba167c84ff
pranavdevadas <pranavdevadas2@gmail.com>
2024-10-02 19:18:20 +0530

    for hosting

--- frontend/src/slice/apiSlice.js
+++ frontend/src/slice/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseUrl = 'https://bookitt.online/api';
+const baseUrl = 'https://bookitt.online';
 
 const baseQuery = fetchBaseQuery({ baseUrl })
 
