880d3652221bebc01e01f913c31bdeb54046e599
pranavdevadas <pranavdevadas2@gmail.com>
2024-10-03 13:44:49 +0530

    for hosting

--- backend/utils/userGenerateToken.js
+++ backend/utils/userGenerateToken.js
@@ -7,8 +7,9 @@ let userGenerateToken = (res, userId) => {
 
     res.cookie('userJwt', token, {
         httpOnly : true,
-        secure : process.env.NODE_ENV !== 'development',
-        sameSite : 'None',
+        //secure : process.env.NODE_ENV !== 'development',
+        secure : true,
+        sameSite : 'Strict',
         maxAge : 30 * 24 * 60 * 60 * 100,
     })
 }
