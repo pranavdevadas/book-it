62ea4de1132c60d728d738710bdf352a2285df18
pranavdevadas <pranavdevadas2@gmail.com>
2024-10-03 16:41:21 +0530

    for debugging

--- backend/utils/userGenerateToken.js
+++ backend/utils/userGenerateToken.js
@@ -4,12 +4,10 @@ let userGenerateToken = (res, userId) => {
     let token = jwt.sign({userId}, process.env.JWT_SECRET_USER, {
         expiresIn : '30d'
     })
-
-    console.log('node.env', process.env.NODE_ENV)
     
     res.cookie('userJwt', token, {
         httpOnly : true,
-        secure : process.env.NODE_ENV !== 'development',
+        secure : true,
         sameSite : 'strict',
         maxAge : 30 * 24 * 60 * 60 * 100
     })
