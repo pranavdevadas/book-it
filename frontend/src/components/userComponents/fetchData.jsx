b2279cf8dc79a12734d9f559f7043ad1a4688282
pranavdevadas <pranavdevadas2@gmail.com>
2024-10-03 18:43:50 +0530

    for debugging

--- frontend/src/components/userComponents/fetchData.jsx
+++ frontend/src/components/userComponents/fetchData.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 export const fetchUserData = async () => {
+    console.log('fetch')
     const response = await axios.get('/api/users/fetchData', {
         withCredentials: true,
-    });
-    console.log(response)
+    })
+    console.log('private')
+    console.log('data', response)
     return response.data;
 };
