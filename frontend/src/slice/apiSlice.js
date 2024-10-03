9aefd2ccb8c713095550f6949377ac92aa4a8c2d
pranavdevadas <pranavdevadas2@gmail.com>
2024-10-03 15:40:12 +0530

    added domain

--- frontend/src/slice/apiSlice.js
+++ frontend/src/slice/apiSlice.js
@@ -4,6 +4,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({ baseUrl : 'https://bookitt.online', credentials: "include" });
 
+console.log(baseQuery)
+
 export const apiSlice = createApi({
   baseQuery,
   tagTypes: ["User"],
