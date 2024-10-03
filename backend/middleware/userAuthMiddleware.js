d9012ee205750413dc90f1e10737b78bc0c2bbbc
pranavdevadas <pranavdevadas2@gmail.com>
2024-10-03 18:34:20 +0530

    for debugging

--- backend/middleware/userAuthMiddleware.js
+++ backend/middleware/userAuthMiddleware.js
@@ -4,13 +4,15 @@ import User from "../model/user.js";
 
 const userProtect = expressAsyncHandler(async (req, res, next) => {
   const token = req.cookies.userJwt;
-  console.log(token)
+
   if (token) {
     try {
       let decoded = jwt.verify(token, process.env.JWT_SECRET_USER);
       let user = await User.findById(decoded.userId).select("-password");
-
+      console.log('decoded',decoded)
+      console.log('user',user)
       if (user.isBlocked) {
+        console.log('hnjhkjj')
         res.clearCookie("userJwt", {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production",
@@ -18,15 +20,18 @@ const userProtect = expressAsyncHandler(async (req, res, next) => {
         res.status(401);
         throw new Error("Your account is blocked. You have been logged out.");
       }
-
+      console.log('njdjhdkj')
       req.user = user;
+      console.log('done')
       next();
     } catch (error) {
+      console.log('erar', error)
       console.error("Token verification failed:", error);
       res.status(401);
       throw new Error("Not Authorized, Invalid token");
     }
   } else {
+    console.log('no Token')
     res.status(401);
     throw new Error("Not Authorized, No token");
   }
--- backend/utils/userGenerateToken.js
+++ backend/utils/userGenerateToken.js
@@ -1,18 +1,18 @@
 import jwt from "jsonwebtoken";
 
 let userGenerateToken = (res, userId) => {
-    console.log('jjjdj')
+
     let token = jwt.sign({userId}, process.env.JWT_SECRET_USER, {
         expiresIn : '30d'
     })
-    console.log('nnnnnn')
+
     res.cookie('userJwt', token, {
         httpOnly : true,
         secure : true,
         sameSite : 'None',
         maxAge : 30 * 24 * 60 * 60 * 100
     })
-    console.log('llljjjj')
+
 }
 
 export default userGenerateToken
