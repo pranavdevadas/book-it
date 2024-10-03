d99528571f4570e9f95e00b0a9858840b0a84888
pranavdevadas <pranavdevadas2@gmail.com>
2024-10-03 18:07:09 +0530

    for debugging

--- backend/utils/userGenerateToken.js
+++ backend/utils/userGenerateToken.js
@@ -1,21 +1,18 @@
 import jwt from "jsonwebtoken";
 
 let userGenerateToken = (res, userId) => {
-    try {
-        let token = jwt.sign({ userId }, process.env.JWT_SECRET_USER, {
-            expiresIn: '30d'
-        });
+    console.log('jjjdj')
+    let token = jwt.sign({userId}, process.env.JWT_SECRET_USER, {
+        expiresIn : '30d'
+    })
+    console.log('nnnnnn')
+    res.cookie('userJwt', token, {
+        httpOnly : true,
+        secure : true,
+        sameSite : 'strict',
+        maxAge : 30 * 24 * 60 * 60 * 100
+    })
+    console.log('llljjjj')
+}
 
-        res.cookie('userJwt', token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: 'strict',
-            maxAge: 30 * 24 * 60 * 60 * 1000
-        });
-    } catch (error) {
-        console.error("Error generating token or setting cookie:", error.message);
-        res.status(500).json({ message: "Internal server error while generating token." });
-    }
-};
-
-export default userGenerateToken;
+export default userGenerateToken
