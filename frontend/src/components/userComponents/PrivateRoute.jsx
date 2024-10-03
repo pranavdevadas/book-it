2363736618b9983ddd492958128fab305097309a
pranavdevadas <pranavdevadas2@gmail.com>
2024-10-03 19:00:35 +0530

    for debugging

--- frontend/src/components/userComponents/PrivateRoute.jsx
+++ frontend/src/components/userComponents/PrivateRoute.jsx
@@ -9,27 +9,27 @@ function PrivateRoute() {
   const { userInfo } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const checkUserStatus = async () => {
-      try {
-        const userData = await fetchUserData();
-        dispatch(setCredentials(userData));  
+  // useEffect(() => {
+  //   const checkUserStatus = async () => {
+  //     try {
+  //       const userData = await fetchUserData();
+  //       dispatch(setCredentials(userData));  
 
-        if (userData && userData.isBlocked) {
-          localStorage.removeItem("userInfo");  
-          dispatch(clearCredentials()); 
-        }
-      } catch (error) {
-        console.log('errordddddd',error)
-        console.error("Failed to fetch user data:", error);
-        toast.error("You are Blocked, Contact Admin for more info")
-        dispatch(clearCredentials());  
-        localStorage.removeItem("userInfo");  
-      }
-    };
+  //       if (userData && userData.isBlocked) {
+  //         localStorage.removeItem("userInfo");  
+  //         dispatch(clearCredentials()); 
+  //       }
+  //     } catch (error) {
+  //       console.log('errordddddd',error)
+  //       console.error("Failed to fetch user data:", error);
+  //       toast.error("You are Blocked, Contact Admin for more info")
+  //       dispatch(clearCredentials());  
+  //       localStorage.removeItem("userInfo");  
+  //     }
+  //   };
 
-    checkUserStatus();
-  }, [dispatch]);
+  //   checkUserStatus();
+  // }, [dispatch]);
 
   return userInfo && !userInfo.isBlocked ? ( <Outlet /> ) : ( <Navigate to="/login" replace /> )
 }
