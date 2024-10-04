731d94db5870138a55022eeb7a919627b93eb25a
pranavdevadas <pranavdevadas2@gmail.com>
2024-10-04 19:30:57 +0530

    for debugging

--- frontend/src/components/userComponents/Carousels.jsx
+++ frontend/src/components/userComponents/Carousels.jsx
@@ -1,60 +1,41 @@
 import Carousel from "react-bootstrap/Carousel";
-import React from "react";
+import React, { useEffect } from "react";
 import ExampleCarouselImage from "../../assets/kalkiposter.jpeg";
 import ExampleCarouselImage1 from "../../assets/batman.jpg";
 import ExampleCarouselImage2 from "../../assets/kali.jpg";
 
-function Carousels({ banner }) {
-  const banner1 = `https://bookitt.online/${banner.banner1}`;
-  const banner2 = `https://bookitt.online/${banner.banner2}`;
-  const banner3 = `https://bookitt.online/${banner.banner3}`;
+function Carousels({ banner, bannerRefetch }) {
+
+  const banner1 = `https://bookitt.online/${banner.banner1}`
+  const banner2 = `https://bookitt.online/${banner.banner2}`
+  const banner3 = `https://bookitt.online/${banner.banner3}`
 
   return (
     <>
-      
-      <Carousel
-        style={{ width: "100%", margin: 0, padding: 0, overflow: 'hidden' }} // Added overflow hidden
-      >
+      <h2 className="text-center mt-3 fw-bold">Book Movies Now</h2>
+      <Carousel className="d-flex justify-content-center mt-5">
         <Carousel.Item>
           <img
-            className="d-block"
+            className="d-block w-75 mx-auto"
             src={banner1 || ExampleCarouselImage}
             alt="First slide"
-            style={{
-              height: "500px",
-              objectFit: "cover",
-              width: "100%",
-              margin: 0,
-              display: "block" // Added display block
-            }}
+            style={{ height: "500px", objectFit: "cover" }}
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block"
-            src={banner2 || ExampleCarouselImage1}
+            className="d-block w-75 mx-auto"
+            src={banner2 ||ExampleCarouselImage1}
             alt="Second slide"
-            style={{
-              height: "500px",
-              objectFit: "cover",
-              width: "100%",
-              margin: 0,
-              display: "block" // Added display block
-            }}
+            style={{ height: "500px", objectFit: "cover" }}
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block"
+            className="d-block w-75 mx-auto"
             src={banner3 || ExampleCarouselImage2}
             alt="Third slide"
-            style={{
-              height: "500px",
-              objectFit: "cover",
-              width: "100%",
-              margin: 0,
-              display: "block" // Added display block
-            }}
+            style={{ height: "500px", objectFit: "cover" }}
           />
         </Carousel.Item>
       </Carousel>
