e00def9a493d53664c447a629e492d9403461ed7
pranavdevadas <pranavdevadas2@gmail.com>
2024-10-04 13:46:54 +0530

    for debugging

--- frontend/src/components/userComponents/OAuth.jsx
+++ frontend/src/components/userComponents/OAuth.jsx
@@ -7,48 +7,40 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 function OAuth() {
-    let dispatch = useDispatch();
-    let navigate = useNavigate();
-    let auth = getAuth(app);
 
-    let handleGoogleClick = async () => {
-        let provider = new GoogleAuthProvider();
-        provider.setCustomParameters({ prompt: "select_account" });
+    let dispatch = useDispatch()
+    let navigate = useNavigate()
 
-        try {
-            let resultFromGoogle = await signInWithPopup(auth, provider);
-            console.log('result from google',resultFromGoogle)
-            let res = await fetch("/api/users/google", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({
-                    name: resultFromGoogle.user.displayName,
-                    email: resultFromGoogle.user.email,
-                }),
-            });
 
-            console.log('res',res)
+  let auth = getAuth(app);
+  let handleGoogleClick = async () => {
+    let provider = new GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: "select_account" });
+    try {
+      let resultFromGoogle = await signInWithPopup(auth, provider);
+      let res = await fetch("/api/users/google", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: resultFromGoogle.user.displayName,
+          email: resultFromGoogle.user.email,
+        }),
+    });
 
-            let data = await res.json();
-            if (res.ok) {
-              console.log('dfdfdfd', res)
-                dispatch(setCredentials(data));
-                navigate('/');
-            } else {
-              console.log('ddfdfd')
-              console.log(data)
-                console.error("Failed to login:", data); // Log any errors received from the server
-            }
-        } catch (error) {
-          console.log('error')
-          console.log(error)
-            console.error("Error during Google sign-in:", error); // Enhanced error logging
-        }
-    };
+    let data = await res.json()
+    if (res.ok) {
+        dispatch(setCredentials(data));
+        navigate('/')
+    }
 
-    return (
-        <GoogleButton style={{ width: "496px" }} onClick={handleGoogleClick} />
-    );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <GoogleButton style={{ width: "496px" }} onClick={handleGoogleClick} />
+  );
 }
 
 export default OAuth;
